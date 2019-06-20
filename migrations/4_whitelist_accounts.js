8a973a3cba9f82dac14ab6cc1c29859a145214d3
James Morgan <james.morgan.e@googlemail.com>
2019-06-20 14:30:41 +0100

    Adding flat contracts and whitelist more accounts

---
+++ migrations/4_whitelist_accounts.js
@@ -0,0 +1,13 @@
+const {getAccountOne} = require('../constants');
+
+const RendarToken = artifacts.require('./RendarToken.sol');
+
+module.exports = async function (deployer, network, accounts) {
+    const renderToken = await RendarToken.deployed();
+
+    // BlockRocket Accounts
+    await renderToken.addWhitelisted("0x04AeC888Fa635c54661581d7bb2CF337c2f1f98F", {from: getAccountOne(accounts, network)});
+
+    // FIXME add Rendar accounts
+
+};
